3f6c918b543b88b5d00cd3d81db492028d3c8892
thinkij <thinkij@levelthree.kr>
2024-02-05 09:23:15 +0900

    임시배포 반영

---
+++ assets/pims copy 7-2wO_JENq.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./WorkContentCommon-nvze9hgw.js";import{d as o,e as s,j as r,g as c}from"./index-shsAaW5a.js";const n={class:"work-container"},m={__name:"pims copy 7",props:{workData:Object},setup(e){const t=e.workData.detail;return(i,l)=>(o(),s("div",n,[r(a,{detail:c(t)},null,8,["detail"])]))}};export{m as default};
